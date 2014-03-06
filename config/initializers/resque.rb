9141580b9cec9fb41de555784aa25058ae242656
Luke Ngakane <luke@lukecreates.com>
2014-03-06 10:00:34 +0000

    tweaked procfile

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,10 +1,11 @@
 ENV["REDISTOGO_URL"] ||= "redis://redistogo:698abf50069280bee73198fcecf18cd4@jack.redistogo.com:9698/"
+ENV['QUEUE'] = '*'
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
-
 Resque.redis.namespace = "resque:propertyshare"
 
 Resque.after_fork do
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+  Resque.redis.namespace = "resque:propertyshare"
 end
