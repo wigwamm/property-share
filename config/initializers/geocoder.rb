5352c6c380ad6d3e45129c45398c6dc7fca3f931
Luke Ngakane <luke@lukecreates.com>
2014-05-03 19:12:11 +0100

    updated geocoder cacheing

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,17 +1,19 @@
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:6f7a4f7d7619a54f34e788e0b610912d@jack.redistogo.com:10167"
+ENV["REDISTOGO_URL"] = "redis://127.0.0.1:6379" if (Rails.env.development?) || (Rails.env.test?)
+uri = URI.parse(ENV["REDISTOGO_URL"])
+
 Geocoder.configure(
 
   # geocoding service (see below for supported options):
-  lookup: :yandex,
+  lookup: :google,
   # IP address geocoding service (see below for supported options):
-  ip_lookup: :maxmind,
+  # ip_lookup: :maxmind,
   # to use an API key:
   # api_key: "...",
   # geocoding service request timeout, in seconds (default 3):
   timeout: 5,
-  # set default units to kilometers:
   units: :mi,
-  # caching (see below for details):
-  cache: $redis,
-  cache_prefix: "geocoder_#{Rails.env}"
+  cache: Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true),
+  cache_prefix: "geocoder:propertyshare:"
 
 )
